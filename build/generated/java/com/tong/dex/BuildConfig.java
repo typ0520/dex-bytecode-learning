7595454e00edb1e3fb899f111ec41c6e9b21c0e7
typ0520 <yapeng.tong@baidao.comc>
2017-12-18 18:25:41 +0800

    temp

---
+++ build/generated/java/com/tong/dex/BuildConfig.java
@@ -0,0 +1,7 @@
+
+package com.tong.dex;
+
+public final class BuildConfig {
+    private BuildConfig() {}
+    public static final String PROJECT_DIR = "/Users/tong/Projects/dex-bytecode-learning";
+}
